6b869bc885e90ea484edfdec2740c3bac05d9db0
1115372936 <30344748+1115372936@users.noreply.github.com>
2023-02-21 15:48:30 +0000

    add pause btns

--- CSC8503/PauseRPass.cpp
+++ CSC8503/PauseRPass.cpp
@@ -42,35 +42,11 @@ PauseRPass::PauseRPass(OGLRenderer& renderer, GameWorld& gameWorld) :
 	pauseTexture->Bind();
 	glUniform1i(glGetUniformLocation(pauseShader->GetProgramID(), "diffuseTex"), 0);
 	pauseTexture->Unbind();
-
-	//text
-	textShader = new OGLShader("debugText.vert", "debugText.frag");
-	textShader->Bind();
-
-	Matrix4 viewProjMatrix = Matrix4::Orthographic(0.0, 100.0f, 100, 0, -1.0f, 1.0f);
-	glUniformMatrix4fv(glGetUniformLocation(textShader->GetProgramID(), "viewProjMatrix"), 1, GL_FALSE, (GLfloat*)viewProjMatrix.array);
-	glUniform1i(glGetUniformLocation(textShader->GetProgramID(), "fontTex"), 0);
-
-	textShader->Unbind();
-
-	glGenVertexArrays(1, &textVAO);
-
-	glGenBuffers(1, &textVertVBO);
-	glGenBuffers(1, &textColourVBO);
-	glGenBuffers(1, &textTexVBO);
-
-	SetDebugStringBufferSizes(10000);
 }
 
 PauseRPass::~PauseRPass() {
 	delete pauseShader;
 	delete pauseTexture;
-
-	glDeleteBuffers(1, &textVertVBO);
-	glDeleteBuffers(1, &textColourVBO);
-	glDeleteBuffers(1, &textTexVBO);
-
-	glDeleteVertexArrays(1, &textVAO);
 };
 
 void PauseRPass::Render() {
@@ -95,97 +71,11 @@ void PauseRPass::Render() {
 		sprintf_s(path, "button%d.jpg", i + 4);
 		pBtn->SetFilename(path);
 		pBtn->SetTexture();
-		pBtn->SetScale(Vector2(0.15f, 0.07f));
+		pBtn->SetScale(Vector2(0.17f, 0.08f));
 		pBtn->SetPosition(Vector2(0, 0.45f - (i * 0.3f)));
 		pBtn->Render();
 	}
 
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
-}
-
-void PauseRPass::SetDebugStringBufferSizes(size_t newVertCount) {
-	if (newVertCount <= textCount) {
-		return;
-	}
-	textCount = newVertCount;
-
-	glBindBuffer(GL_ARRAY_BUFFER, textVertVBO);
-	glBufferData(GL_ARRAY_BUFFER, textCount * sizeof(Vector3), nullptr, GL_DYNAMIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, textColourVBO);
-	glBufferData(GL_ARRAY_BUFFER, textCount * sizeof(Vector4), nullptr, GL_DYNAMIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, textTexVBO);
-	glBufferData(GL_ARRAY_BUFFER, textCount * sizeof(Vector2), nullptr, GL_DYNAMIC_DRAW);
-
-	debugTextPos.reserve(textCount);
-	debugTextColours.reserve(textCount);
-	debugTextUVs.reserve(textCount);
-
-	glBindVertexArray(textVAO);
-
-	glVertexAttribFormat(0, 3, GL_FLOAT, false, 0);
-	glVertexAttribBinding(0, 0);
-	glBindVertexBuffer(0, textVertVBO, 0, sizeof(Vector3));
-
-	glVertexAttribFormat(1, 4, GL_FLOAT, false, 0);
-	glVertexAttribBinding(1, 1);
-	glBindVertexBuffer(1, textColourVBO, 0, sizeof(Vector4));
-
-	glVertexAttribFormat(2, 2, GL_FLOAT, false, 0);
-	glVertexAttribBinding(2, 2);
-	glBindVertexBuffer(2, textTexVBO, 0, sizeof(Vector2));
-
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-
-	glBindVertexArray(0);
-}
-
-void PauseRPass::RenderText() {
-	const std::vector<Debug::DebugStringEntry>& strings = Debug::GetDebugStrings();
-	if (strings.empty()) {
-		return;
-	}
-	textShader->Bind();
-
-	OGLTexture* texture = (OGLTexture*)Debug::GetDebugFont()->GetTexture();
-
-	if (!texture) {
-		return;
-	}
-	texture->Bind(0);
-
-	debugTextPos.clear();
-	debugTextColours.clear();
-	debugTextUVs.clear();
-
-	int frameVertCount = 0;
-	for (const auto& s : strings) {
-		frameVertCount += Debug::GetDebugFont()->GetVertexCountForString(s.data);
-	}
-	SetDebugStringBufferSizes(frameVertCount);
-
-	for (const auto& s : strings) {
-		float size = 20.0f;
-		Debug::GetDebugFont()->BuildVerticesForString(s.data, s.position, s.colour, size, debugTextPos, debugTextUVs, debugTextColours);
-	}
-
-	Debug::Print("Start", Vector2(55, 50), Vector4(0, 0, 0, 1));
-
-	glBindBuffer(GL_ARRAY_BUFFER, textVertVBO);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, frameVertCount * sizeof(Vector3), debugTextPos.data());
-	glBindBuffer(GL_ARRAY_BUFFER, textColourVBO);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, frameVertCount * sizeof(Vector4), debugTextColours.data());
-	glBindBuffer(GL_ARRAY_BUFFER, textTexVBO);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, frameVertCount * sizeof(Vector2), debugTextUVs.data());
-
-	glBindVertexArray(textVAO);
-	glDrawArrays(GL_TRIANGLES, 0, frameVertCount);
-	glBindVertexArray(0);
-
-
-	textShader->Unbind();
 }
