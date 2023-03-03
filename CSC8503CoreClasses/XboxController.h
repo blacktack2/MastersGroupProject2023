f2dab7260953761a30603bcb55d603d9fe5df0c1
Felix Chiu <c2061500@newcastle.ac.uk>
2023-03-03 15:33:04 +0000

    Partial merge with xbox

---
+++ CSC8503CoreClasses/XboxController.h
@@ -0,0 +1,40 @@
+/**
+ * @file   XboxController.h
+ * @brief  header file for Xbox controller
+ *
+ * @author Shantao Liu
+ * @author Xiaoyang Liu
+ * @date   February 2023
+ */
+
+#pragma once
+
+#include<Windows.h>
+#include<Xinput.h>
+#include <iostream>
+#include <vector>
+
+namespace NCL
+{
+    class XboxController
+    {
+    public:
+        XboxController();
+
+        ~XboxController();
+
+
+        bool GetThumbLeft(unsigned int controllerNum, Maths::Vector2& v);
+        bool GetThumbRight(unsigned int controllerNum, Maths::Vector2& v);
+        bool GetRightTrigger(unsigned int controllerNum, float& n);
+        bool GetLeftTrigger(unsigned int controllerNum, float& n);
+
+    protected:
+
+        bool UpdateConnection(unsigned int controllerNum);
+
+        XINPUT_STATE state;
+        DWORD dwResult;
+        XINPUT_VIBRATION vibration;
+    };
+}
